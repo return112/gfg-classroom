8b857ee42881d154ea29068c8dfba03c9cfcdd97
shubham <shubhampal2002.ldh@gmail.com>
2025-03-07 13:18:32 +0530

    learn JS

---
+++ javascript/script.js
@@ -0,0 +1,37 @@
+// var a = 1;
+// console.log(a);
+// var a = "hello";
+// console.log(a);
+// var a = true;
+// console.log(a);
+
+// var a = 1;
+// console.log(typeof a);
+// var a = "hello";
+// console.log(typeof a);
+// var a = true;
+// console.log(typeof a);
+
+// var b ;
+// console.log(b);
+
+// var c =null;
+// console.log(typeof  c);
+
+
+// redeclaration
+// var a = 10;
+// console.log(a);
+// var a = "hello";
+// console.log(a);
+
+// reinitialization
+// const a = 10;
+// console.log(a);
+// let a = "hello";
+// console.log(a);
+
+var a = 10;
+console.log(a);
+ a = "hello";
+console.log(a);
